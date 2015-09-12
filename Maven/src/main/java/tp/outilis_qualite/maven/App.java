a4dbadb3a439a9553c93ef600f200435c5bee26f
johndeco <johnmarcdechaudf@gmail.com>
2015-09-12 23:20:30 +0200

    First commit

---
+++ Maven/src/main/java/tp/outilis_qualite/maven/App.java
@@ -0,0 +1,13 @@
+package tp.outilis_qualite.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
